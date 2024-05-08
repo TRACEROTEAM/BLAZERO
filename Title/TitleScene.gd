b01d5c2def82ad7b533326af2fa151dbf1a9ae5b
低耐久的铁砧 <67966070+Ironnoob73@users.noreply.github.com>
2024-05-08 13:44:06 +0800

    20240508DU1
    Login Screen

--- Title/TitleScene.gd
+++ Title/TitleScene.gd
@@ -3,12 +3,16 @@ var current_menu : String = "Ready"
 
 @onready var animation = $AnimationPlayer
 @onready var timer = $Main/HBoxContainer/Timer
+@onready var login_window = $LoginWindow
+@onready var setting_and_qq = $SettingAndQq
 
 func _input(event):
-	if current_menu == "Ready" and (event is InputEventKey or event is InputEventMouseButton):
-		animation.play("ToMainMenu")
-		current_menu = "Main"
-	timer.start()
+	if current_menu == "Ready" \
+	and (event is InputEventKey or event is InputEventMouseButton) \
+	and !Rect2(setting_and_qq.position, setting_and_qq.size).has_point(get_local_mouse_position()):
+		login_window.position.x = get_window().size.x/2 - login_window.size.x/2
+		login_window.position.y = get_window().size.y/2 - login_window.size.y/2
+		login_window.show()
 		
 func _on_timer_timeout():
 	if current_menu == "Main":
@@ -40,3 +44,12 @@ func _on_confirm_button_pressed():
 #World
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+func _on_login_button_pressed():
+	login_window.hide()
+	setting_and_qq.hide()
+	animation.play("ToMainMenu")
+	current_menu = "Main"
+
+func _on_login_window_close_requested():
+	login_window.hide()
